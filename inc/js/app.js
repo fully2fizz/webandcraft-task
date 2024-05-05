00eda4520b29c5e19fb6b114c494722ce6566bab
fully2fizz <128681884+fully2fizz@users.noreply.github.com>
2024-05-05 22:38:30 +0530

    fixes

--- inc/js/app.js
+++ inc/js/app.js
@@ -57,32 +57,22 @@ if (window.matchMedia("(min-width: 992px)").matches) {
 // ==========================================
 // ================= Responsive Navbar ================= 
 // ==========================================
-$(document).ready(function(){
-  $('.menu-toggle').click(function() {
-      if($('.menu').hasClass("-open")){
-          TweenMax.to([".menu-backdrop"], 1,{autoAlpha:0});
-          TweenMax.to([".menu-content"], 1,{x:'100%'});
-          TweenMax.to([".menu-body"], 1,{x:'100%',autoAlpha:0});
-          TweenMax.to([".menu-footer"], 1,{x:'100%',autoAlpha:0});
-      }
-      else {
-          TweenMax.to([".menu-backdrop"], 1,{autoAlpha:1});
-          TweenMax.to([".menu-content"], 1,{x:'0%'});
-          TweenMax.to([".menu-body"], 1,{x:'0%',autoAlpha:1});
-          TweenMax.to([".menu-footer"], 1,{x:'0%',autoAlpha:1});
-          $('.menu-box').css("display","block");
-      }
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleBtn = document.querySelector('.toggle-btn');
+  const closeBtn = document.querySelector('.close-btn');
+  const responsiveNav = document.querySelector('.responsive-nav');
+
+  toggleBtn.addEventListener('click', function () {
+      responsiveNav.classList.add('active');
   });
-  $(".menu-toggle").click(function(){
-      $('.menu').toggleClass("-open");
+
+  closeBtn.addEventListener('click', function () {
+      responsiveNav.classList.remove('active');
   });
 });
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 30) {
-      return document.querySelector('.menu').classList.add('active')
-  }
-  return document.querySelector('.menu').classList.remove('active')
-});
+
+
+
 
 
 
